5489aedd5fcdd9afbb68759a7ff18e2f6a1c8e22
yutaro_elk <36399275+yutaro-elk@users.noreply.github.com>
2023-02-23 20:13:32 +0900

    update

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from './error-page';
 import './index.css';
 import Contact from './routes/contact';
-import Root from './routes/root';
+import Root, { loader as rootLoader } from './routes/root';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <Root />,
 		errorElement: <ErrorPage />,
+		loader: rootLoader,
 		children: [
 			{
 				path: 'contacts/:contactId',
